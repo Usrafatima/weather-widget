073a2d4d388b5976c371651258dbc34b3db4a111
Yusra <yusra7003@gmail.com>
2024-09-07 00:09:27 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import WeatherWidget from "@/components/weather-widget";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+     <WeatherWidget/>
+    </div>
+    
+  )
+}
